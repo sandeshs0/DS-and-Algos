770cb3fd032efc765920de90f41757eb7ee63d55
Ashok Shah <121223930+bayes-ashok@users.noreply.github.com>
2024-03-13 19:55:00 +0545

    Add files via upload

---
+++ examdsa/KthMissingEven.java
@@ -0,0 +1,32 @@
+public class KthMissingEven {
+
+    public static int findMissingKthEven(int[] arr, int k) {
+        int count = 0;
+        int num = arr[0];
+        
+        for (int i = 0; i < arr.length; i++) {
+            while (num < arr[i]) {
+                count++;
+                if (count == k) {
+                    return num;
+                }
+                num += 2;
+            }
+            num += 2;
+        }
+        
+        // If kth missing even is beyond the end of the array
+        while (count < k) {
+            count++;
+            num += 2;
+        }
+        
+        return num - 2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 10, 14};
+        int k = 2;
+        System.out.println("The " + k + "nd missing even number is: " + findMissingKthEven(arr, 3));
+    }
+}
